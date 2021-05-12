9f600706999f233270dd622d9c63d0586f1dabc3
Mingot Virgil <61415049+globedjill@users.noreply.github.com>
2021-05-12 18:01:24 +0200

    re

--- Backend/environment/prod.js
+++ Backend/environment/prod.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = {
     dbUrl: 'mongodb+srv://virgil_admin:Ser0t7aTsqhXGdRH@cluster0.z3wus.mongodb.net/monSite?retryWrites=true&w=majority',
-    cert: path.join(__dirname, ''),
-    key: path.join(__dirname, '')
+    cert: '/etc/letsencrypt/live/www.cv.spherik.fr/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.cv.spherik.fr/privkey.pem'
 }
