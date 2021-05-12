fd2a1cae679aea7252044dd5a59e99ef8583dd86
Mingot Virgil <61415049+globedjill@users.noreply.github.com>
2021-05-12 21:02:47 +0200

    format prod

--- Backend/app.js
+++ Backend/app.js
@@ -27,8 +27,8 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, 'public/img')));
 
 app.get('*', (req, res) => {
-    // res.sendFile(path.join(__dirname + './../public/index.html'));
-    res.sendFile(path.join(__dirname + './../Frontend/src/index.html'));
+    res.sendFile(path.join(__dirname + './../public/index.html'));
+    // res.sendFile(path.join(__dirname + './../Frontend/src/index.html'));
 });
 
 
